3743a047529a46f1970e8756a5226318a47d1470
lajoskisslk2007 <kisslajos20070829@gmail.com>
2026-09-10 11:45:04 +0200

    negyedik feladat

---
+++ Jatekos/Jatekos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Text;
+
+namespace Jatekosok
+{
+    public class Jatekos
+    {
+        private int elet;
+        public string nev { get; set; } 
+        public int Elet
+        {
+            get { return elet; }
+            set
+            {
+                if (value < 0)
+                {
+                    elet = 0;
+                }
+                else if (value > 100)
+                {
+                    elet = 100;
+                }
+                else
+                {
+                    elet = value;
+                }
+            }
+        }
+    }
+}
---
+++ Jatekos/Program.cs
@@ -0,0 +1,7 @@
+﻿using Jatekosok;
+
+Jatekos jatekos = new Jatekos();
+
+jatekos.nev = "Játékos1";
+jatekos.Elet = 20;
+Console.WriteLine($"név:{jatekos.nev},Élet:{jatekos.Elet}");
