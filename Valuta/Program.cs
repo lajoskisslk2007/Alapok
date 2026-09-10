9d998911d4e32239ce090aa3394a3a2c51fd65d6
lajoskisslk2007 <kisslajos20070829@gmail.com>
2026-09-10 11:13:22 +0200

    második feladat

---
+++ Valuta/Program.cs
@@ -0,0 +1,9 @@
+﻿using Valuta;
+
+ValutaValto peldany = new ValutaValto();
+
+peldany.Arfolyam = 370;
+
+Console.WriteLine($"Euró:100,Árfolyam:{ peldany.EurorolHuf(100)}Ft");
+
+Console.WriteLine($"Euró:100,Árfolyam:{ValutaValto.FixEurorolHuf(100)}");
---
+++ Valuta/ValutaValto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Valuta
+{
+    public class ValutaValto
+    {
+        public double Arfolyam;
+        public double EurorolHuf(double euro)
+        {
+
+            return euro * Arfolyam;
+        }
+        public static double FixEurorolHuf(double euro)
+        {
+         
+            return euro * 400;
+        }
+
+    }
+}
