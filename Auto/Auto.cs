f7bf29cbc188aa95c43ca9a30531bc8a0f7fa282
lajoskisslk2007 <kisslajos20070829@gmail.com>
2026-09-10 10:34:57 +0200

    első feladat

---
+++ Auto/Auto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auto
+{
+    public class Autok
+    {
+        public string marka;
+        public int Eletkor;
+
+    }
+}
--- Auto/Program.cs
+++ Auto/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Auto;
+
+Autok piros =new Autok();
+piros.marka = "toyota";
+piros.Eletkor = 5;
+
+Autok kek=new Autok();
+Console.Write("Kérem az autó márkáját:");kek.marka=Console.ReadLine();
+Console.Write("Kérem az autó Életkorát:");kek.Eletkor=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"1.példány:{piros.marka},${piros.Eletkor}Életkor");
+Console.WriteLine($"2.példány:{kek.marka},${kek.Eletkor}Életkor");
+Console.ReadKey();
