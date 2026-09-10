d8a3dec69e81089eb434e67e5b0995a16341fd64
lajoskisslk2007 <kisslajos20070829@gmail.com>
2026-09-10 11:32:42 +0200

    harmadik feladat

---
+++ Bankszamla/Bankszla.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bankszamla
+{
+    internal class Bankszla
+    {
+        private double egyenleg=10000;
+
+        public void befizetés(double osszeg)
+        {
+            if (osszeg > 0)
+            {
+                egyenleg += osszeg;
+                Console.WriteLine($"Befizetés:{osszeg} Ft. új egyenleg:{egyenleg}");
+            }
+            else
+            {
+                Console.WriteLine("A befizetés összege nem lehet negatív.");
+            }
+        }
+        public void kivet(double osszeg)
+        {
+            if (osszeg > 0 && osszeg <= egyenleg)
+            {
+                egyenleg -= osszeg;
+                Console.WriteLine($"Kifizetés: {osszeg} Ft. új egyenleg: {egyenleg} Ft.");
+            }
+            else
+            {
+                Console.WriteLine("A kifizetés összege nem lehete negatív vagy nagyobb, mint az egyenleg");
+            }
+        }
+    }
+}
---
+++ Bankszamla/Program.cs
@@ -0,0 +1,17 @@
+﻿using Bankszamla;
+
+Console.WriteLine("Befizetés:1 ;Kifizetés:2");
+Bankszla peldany = new Bankszla();
+string irany=Console.ReadLine();
+if (irany =="1")
+{
+    Console.WriteLine("Mekkora összeget szeretne befizetni?");
+    double be = Convert.ToDouble(Console.ReadLine());
+    peldany.befizetés(be);
+}
+else
+{
+    Console.WriteLine("Mekkora összeget szeretne kifizetni?");
+    double ki= Convert.ToDouble(Console.ReadLine());
+    peldany.kivet(ki);
+}
